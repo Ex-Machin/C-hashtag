860a66daa374b6a077735e5a982b9609d49dd283
Ex-Machin <grinpigrin@gmail.com>
2022-03-01 11:47:33 +0100

    first c# project

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Cuboid 
+{
+    public int a = 0;
+    public int b = 0;
+    public int h = 0;
+
+    public int Volume()
+    {
+        return a * b * h;
+    }
+}
+
+public class Program 
+{
+    public static void Main()
+    {
+        Cuboid cuboid = new Cuboid();
+        cuboid.a = 1;
+        cuboid.b = 2;
+        cuboid.h = 45;
+        Console.WriteLine(cuboid.Volume());
+    }
+}
