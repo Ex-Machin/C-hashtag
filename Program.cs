a539c44d9a30f98a4bdf68b9a296dca17c8d952f
Ex-Machin <grinpigrin@gmail.com>
2022-04-02 13:43:27 +0200

    switch statement

--- Program.cs
+++ Program.cs
@@ -10,31 +10,42 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-            
-            Console.Write("Enter operator: ");            
-            string op = Console.ReadLine();
+            Console.WriteLine(GetDay(4));
+        }
+        static string GetDay(int dayNum) 
+        {
+            string dayName = "0";
 
-            Console.Write("Enter a number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-        
-            if (op == "+") 
+            switch (dayNum) 
             {
-                Console.WriteLine(num1 + num2);
-            } else if (op == "-") 
-            {
-                Console.WriteLine(num1 - num2);
-            } else if (op == "/") 
-            {
-                Console.WriteLine(num1 / num2);
-            } else if (op == "*") 
-            {
-                Console.WriteLine(num1 * num2);
-            } else 
-            {
-                Console.WriteLine("Invalid operator");
+                case 0:
+                    dayName = "Sunday";
+                    break;        
+                case 1: 
+                    dayName = "Monday";
+                    break;
+                case 2: 
+                    dayName = "Tuesday";
+                    break;
+                case 3: 
+                    dayName = "Wednesday";
+                    break;
+                case 4: 
+                    dayName = "Thursday";
+                    break;
+                case 5: 
+                    dayName = "Friday";
+                    break;
+                case 6: 
+                    dayName = "Saturda";
+                    break;
+                default:
+                    Console.WriteLine("Wrong value");
+                  break;
+
             }
+
+            return dayName;
         }
     }
 }
