809214e73e2c67db00c99ee4ab3fecdeb3ebe0f5
Ex-Machin <grinpigrin@gmail.com>
2022-04-02 11:34:27 +0200

    Working with strings

--- Program.cs
+++ Program.cs
@@ -10,12 +10,23 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            string phrase = "Giraffe \nAcademy";
-            Console.WriteLine(phrase.Contains("Academy"));
-            Console.WriteLine(phrase.ToUpper());
-            Console.WriteLine(phrase[0]);
-            Console.WriteLine(phrase.IndexOf("Z"));
-            Console.WriteLine(phrase.Substring(8, 3));
+            int num = 6;
+            num++;
+
+             Console.WriteLine(50);
+             Console.WriteLine( 6 / 3);
+             Console.WriteLine( 5 % 2);
+             Console.WriteLine( 4.0 + 8);
+             Console.WriteLine( 5 / 2 );
+             Console.WriteLine( 5 / 2.0 );
+             Console.WriteLine(num);
+             Console.WriteLine( Math.Abs(-30) );
+             Console.WriteLine( Math.Pow(3,2));
+             Console.WriteLine( Math.Sqrt(36));
+             Console.WriteLine( Math.Max(4, 90));
+             Console.WriteLine(Math.Round(1.59));
+             Console.WriteLine();
+             Console.WriteLine();
         }
     }
 }
