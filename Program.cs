f967baeadde70d1f4bc7d791d48c1d758e78515c
Ex-Machin <grinpigrin@gmail.com>
2022-04-02 11:00:48 +0200

    data types

--- Program.cs
+++ Program.cs
@@ -10,16 +10,7 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            string characterName = "Tom";
-            int characterAge = 25;
-
-            Console.WriteLine("There once was a man named " + characterName);
-            Console.WriteLine("He was " + characterAge + " years old");
-
-            characterName = "Mike";
-
-            Console.WriteLine("He really liked the name " + characterName);
-            Console.WriteLine("But he didn't like being " + characterAge);
+            
         }
     }
 }
