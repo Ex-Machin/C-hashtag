693f396a046c666dfb66c8a10cf40a02746b57ff
Ex-Machin <grinpigrin@gmail.com>
2022-04-02 16:17:25 +0200

    while loop

--- Program.cs
+++ Program.cs
@@ -10,42 +10,12 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetDay(4));
-        }
-        static string GetDay(int dayNum) 
-        {
-            string dayName = "0";
-
-            switch (dayNum) 
+            int index = 6;
+            do
             {
-                case 0:
-                    dayName = "Sunday";
-                    break;        
-                case 1: 
-                    dayName = "Monday";
-                    break;
-                case 2: 
-                    dayName = "Tuesday";
-                    break;
-                case 3: 
-                    dayName = "Wednesday";
-                    break;
-                case 4: 
-                    dayName = "Thursday";
-                    break;
-                case 5: 
-                    dayName = "Friday";
-                    break;
-                case 6: 
-                    dayName = "Saturda";
-                    break;
-                default:
-                    Console.WriteLine("Wrong value");
-                  break;
-
-            }
-
-            return dayName;
+                Console.WriteLine(index);
+                index++;
+            } while (index <= 5);
         }
     }
 }
