ed065a4280653f50d3c4a18d90f995b5b3319729
Ex-Machin <grinpigrin@gmail.com>
2022-04-02 11:01:06 +0200

    data types

--- Program.cs
+++ Program.cs
@@ -10,7 +10,11 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            
+            string phrase = "Giraffe Academy";
+            char grade = 'A';
+            int age = -30;
+            double gpa  = 3.5;
+            bool isMale = true;
         }
     }
 }
