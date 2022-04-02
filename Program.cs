c20becda867b16d2265061a6adb16b70d7d61de1
Ex-Machin <grinpigrin@gmail.com>
2022-04-02 11:41:45 +0200

    Getting user Input

--- Program.cs
+++ Program.cs
@@ -10,23 +10,11 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            int num = 6;
-            num++;
-
-             Console.WriteLine(50);
-             Console.WriteLine( 6 / 3);
-             Console.WriteLine( 5 % 2);
-             Console.WriteLine( 4.0 + 8);
-             Console.WriteLine( 5 / 2 );
-             Console.WriteLine( 5 / 2.0 );
-             Console.WriteLine(num);
-             Console.WriteLine( Math.Abs(-30) );
-             Console.WriteLine( Math.Pow(3,2));
-             Console.WriteLine( Math.Sqrt(36));
-             Console.WriteLine( Math.Max(4, 90));
-             Console.WriteLine(Math.Round(1.59));
-             Console.WriteLine();
-             Console.WriteLine();
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + ", you are " + age);
         }
     }
 }
