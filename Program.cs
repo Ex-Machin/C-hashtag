8e7070ccabee3487c43a0deb3de70fa7de8d5d79
Ex-Machin <grinpigrin@gmail.com>
2022-04-02 12:16:18 +0200

    Building a Calculator

--- Program.cs
+++ Program.cs
@@ -10,11 +10,17 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
-            Console.Write("Enter your age: ");
-            string age = Console.ReadLine();
-            Console.WriteLine("Hello " + name + ", you are " + age);
+            int num = Convert.ToInt32("45");
+            Console.WriteLine(num + 5);
+
+
+            Console.Write("Enter a number: ");
+            double num1 =  Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter another number: ");
+            double num2 =  Convert.ToDouble(Console.ReadLine());
+               
+             Console.WriteLine(num1 + num2);  
         }
     }
 }
